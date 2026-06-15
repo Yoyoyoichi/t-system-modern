eb2769c131e024405f95d657dd32a55645b8762b
T-System Dev <t-system@example.com>
2026-06-15 16:36:31 +0900

    Add script to check tables

---
+++ check_tables.php
@@ -0,0 +1,8 @@
+﻿<?php
+require_once 'db_wrapper.php';
+\ = new db_wrapper();
+\ = \->query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'");
+while (\ = \->fetch_assoc()) {
+    echo \['table_name'] . "\n";
+}
+?>
