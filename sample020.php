1c359ccc9f96406d9597cf91513dc5eb24e85678
T-System Dev <t-system@example.com>
2026-06-24 13:34:27 +0900

    Reset Advanced Search when ctg1 is changed

--- sample020.php
+++ sample020.php
@@ -2831,7 +2831,24 @@ function intRandom(min, max){
 }
 
 async function listChange(categorySelect) {
-    console.log("Triggered by:", categorySelect.id);
+    console.log("Triggered by:", categorySelect?.id);
+    if (categorySelect && categorySelect.id === 'ctg1') {
+        const resetVal = (id, val) => { const el = document.getElementById(id); if (el) el.value = val; };
+        resetVal('wordSearch', '');
+        resetVal('category4', '*');
+        resetVal('operator1', '=');
+        resetVal('criteria1', '');
+        resetVal('category5', '*');
+        resetVal('operator2', '=');
+        resetVal('criteria2', '');
+        resetVal('category6', '*');
+        resetVal('operator3', '=');
+        resetVal('criteria3', '');
+        
+        if (typeof $ !== 'undefined' && $.fn.chosen) {
+            $('#category4, #operator1, #category5, #operator2, #category6, #operator3').trigger('chosen:updated');
+        }
+    }
     firstRemoveFlag = false;
     num = 0;
     flag1 = false;
