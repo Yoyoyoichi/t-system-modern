a5c60c35788691df9c3599dd5d3d61f3b1be3e38
T-System Dev <t-system@example.com>
2026-06-29 15:01:05 +0900

    Fix missing autoread checkbox that caused JS crash

--- sample020.php
+++ sample020.php
@@ -1821,17 +1821,7 @@ word-wrap:break-word;'></pre>
             <span class="msc-checkbox-label">問題/解答</span>
         </label>
         <label class="msc-checkbox-wrapper">
-            <select class="selectBox" name='autoReading' id='autoReading' onchange = "settingSave()" style='width: 23%; font-size: 20px;'>
-    <option value='je' style='width: 23%' placeholder="読上音声">読上音声</option>
-    <option value='je'>問題-日本語/解答‐英語</option>
-    <option value='ej'>問題-英語/解答‐日本語</option>
-    <option value='jj'>問題-日本語/解答‐日本語</option>
-    <option value='ee'>問題-英語/解答‐英語</option>
-    <option value='j*'>問題だけ-日本語</option>
-    <option value='e*'>問題だけ-英語</option>
-    <option value='*j'>解答だけ‐日本語</option>
-    <option value='*e'>解答だけ‐英語</option>
-  </select>
+            <input class="msc-checkbox" type="checkbox" id="autoread" onchange="settingSave()">
             <span class="msc-checkbox-label">自動読上</span>
         </label>
         <label class="msc-checkbox-wrapper">
